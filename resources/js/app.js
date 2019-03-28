87c9355087f748507fddc619b5b1ce5044af1f0a
Kelvin <kelvinkyendwa@gmail.com>
2019-03-28 09:58:08 +0300

    routes

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,9 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import routes from './routes';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const app = new Vue({
-    el: '#app',
+   	 el: '#app',
      router: new VueRouter(routes),
 });
