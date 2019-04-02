9257c15480871886e47cbbaee0fc9e498231cfe7
Aaki Consultants <aakiconsultants@kithinji-pc.aaki.com>
2019-04-02 09:13:07 +0300

    all

--- public/js/app.js
+++ public/js/app.js
@@ -15576,8 +15576,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! c:\xampp\htdocs\spa\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! c:\xampp\htdocs\spa\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/aakiconsultants/Desktop/Mitau/spa/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/aakiconsultants/Desktop/Mitau/spa/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
---
+++ resources/views/app.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Spa</title>
+
+        <link rel="stylesheet" href="/css/app.css">
+
+    </head>
+    <body>
+
+
+    <div id="app">
+        <div class="container mx-auto">
+            <header class="py-6">
+                <h1>App </h1>
+            </header>
+            <main class="flex">
+                <aside class="w-1/5">
+                    <ul>
+                        <li><router-link to ="/">Home</router-link></li>
+                        <li><router-link to ="/about">About</router-link></li>
+                    </ul>
+                
+                </aside>
+
+                <div class="primary flex-1">
+                    <router-view></router-view>
+                </div>
+            </main>
+        
+        </div>
+        
+    </div>
+          
+    <script src="/js/app.js"></script>
+    </body>
+</html>
